d25d4b2857653ab8d27a2301980de17ca1ac92ce
hristiyana <96460833+hristiyana01@users.noreply.github.com>
2024-02-02 06:29:17 +0200

    creating the main porject
    with main dependencies like Lombok, SQL Driver, Flyway Migration, Spring Data JPA, Spring Data JDBC

---
+++ src/test/java/com/example/hometoservice/HomeToServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hometoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeToServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
